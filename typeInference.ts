a1abfea9ff265f91ce9d1d6e700c723dfd5bab1e
ydh0520 <ydh2338@naver.com>
2019-07-02 15:18:52 +0900

    타입추론

---
+++ typeInference.ts
@@ -0,0 +1,12 @@
+//let의 경우 타입추론에의해 타입이 설정된다.
+let a: string = "let type";
+let b = "let type";
+
+/*
+const의 경우 명시하지 않은 const변수에 대하여 리터럴 타입으로 설정한다.
+*/
+
+const c: string = "String 타입";
+const d = "리터럴 타입";
+
+
