92d1a64b15257c1ffa67d57f094300af4242adce
ydh0520 <ydh2338@naver.com>
2019-07-03 13:23:40 +0900

    interface

--- interface.ts
+++ interface.ts
@@ -37,6 +37,20 @@ const persionOption: PersonOption = {
 persionOption[0] = "hi";
 persionOption[100] = "hello";
 
+/*
+주의할 점
+*/
+
+interface indexableProblem{
+    [index:string]:string;
+    /*
+    name: number; 인덱서블 타임이 String이므로 사용할수 없다.
+    name이라는 변수가 indexalbe에해당하는 값인지 고유 값인지 구분할수 없으므로 불가능
+    */
+}
+
+
+
 
 /*
 (코딩 스타일)인터페이스 인식을 편하게 하기 위해 I를 붙인다.
@@ -54,4 +68,23 @@ class subCalss implements IPerson {
     hello():void {
         console.log(`안녕하세요 ${this.name}입니다`);
     }
+
+    hi():void{
+        console.log(`안녕 ${this.name}임`)
+    }
 }
+
+//inter에에는 hi를 가지고 있지 않다.
+const personSub1: subCalss =new subCalss('Mark');
+const personInter: IPerson =new subCalss('Mark');
+
+//interface도 상속이 가능하다.
+interface Korean extends Person{
+    city: string;
+}
+
+const koreanPerson: Korean={
+    name: '홍길동',
+    age: 26,
+    city: '서울'
+};
